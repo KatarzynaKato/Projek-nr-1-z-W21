a7d2b554de57bf9f751059927f944aa63b72259b
Kasia <Kasia@L15-KASIA>
2023-03-11 14:49:21 +0100

    Zadanie dzień 4

--- Projekt nr 1 z W21/Projekt nr 1 z W21/Program.cs
+++ Projekt nr 1 z W21/Projekt nr 1 z W21/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string name = "Ewa";
+string sex = "kobieta";
+int age = 33;
+
+if (sex == "kobieta")
+{
+    if (name == "Ewa" && age == 33) 
+    {
+        Console.WriteLine("Ewa lat 33");
+    }
+    else if (name != "Ewa" && age > 30) 
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+}
+
+if (sex != "kobieta" && age > 18) 
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
